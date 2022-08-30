13042abeb2f6b740c9fcffaf58ba4d70212a348c
quangthuan <hoangquangthuan952@gmail.com>
2022-08-31 00:15:44 +0700

    create new users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,26 @@
 import db from "../models/index";
-
+import CRUDService from "../services/CRUDService"
 let getHomePage = async (req, res) => {
-   try {
-    let data = await db.User.findAll();
-    return res.render('homePage.ejs', {
-        data: JSON.stringify(data)
-    })
-   }
-   catch(err) {
-    console.log(err);
-   }
+    try {
+        let data = await db.User.findAll();
+        return res.render('homePage.ejs', {
+            data: JSON.stringify(data)
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
+let getCRUD = (req, res) => {
+    return res.render("crud.ejs")
+}
+
+let postCRUD = async (req, res) => {
+    const msg = await CRUDService.createNewUser(req.body)
+    return res.send(msg)
+}
 module.exports = {
-    getHomePage: getHomePage
+    getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
